d47604cd8a053b3ca4340408ffe61a532edb50ed
Cyril Marceau <cmarceau@emendo.fr>
2024-07-26 00:28:19 +0200

    fix(Intelephense): warning on laravel method

--- app/Repositories/Auth/AuthRepository.php
+++ app/Repositories/Auth/AuthRepository.php
@@ -7,19 +7,26 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 
-class AuthRepository implements AuthRepositoryInterface {
-    public function login(array $credentials): array {
+class AuthRepository implements AuthRepositoryInterface
+{
+    public function login(array $credentials): array
+    {
         if (!Auth::attempt($credentials)) {
             return ['general_error' => ['Your credentials are invalid']];
         }
 
         $user = Auth::user();
+
+        /**
+         * @disregard
+         */
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return ['token' => $token, 'user' => $user];
     }
 
-    public function signup(array $data): mixed {
+    public function signup(array $data): mixed
+    {
         return User::create([
             'firstname' => $data['firstname'],
             'lastname' => $data['lastname'],
@@ -27,8 +34,12 @@ class AuthRepository implements AuthRepositoryInterface {
             'password' => Hash::make($data['password']),
         ]);
     }
-    
-    public function logout(): mixed {
+
+    public function logout(): mixed
+    {
+        /**
+         * @disregard
+         */
         return Auth::user()->tokens()->delete();
     }
 }
